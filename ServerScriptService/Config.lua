2e8c4157c7163b369bc8bf0429f78eb027daba14
V3N0ML0RD <thoughtstreamecho@gmail.com>
2025-07-09 12:18:07 +0400

    Create Config.lua

---
+++ ServerScriptService/Config.lua
@@ -0,0 +1,18 @@
+return {
+    GamepassIDs = {
+        SpeedBoost = 12345678,
+        FlashlightUpgrade = 23456789
+    },
+
+    DevProductIDs = {
+        Revive = 34567890
+    },
+
+    BadgeIDs = {
+        FirstDeath = 45678901,
+        Escape = 56789012
+    },
+
+    MaxPlayers = 6,
+    StartingLives = 3
+}
